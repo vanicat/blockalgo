444e5e47481ef182c6aa2af807ba7300cb1ba4b0
Rémi Vanicat <vanicat@debian.org>
2016-06-10 12:40:45 +0200

    IO async

--- algoWeb.js
+++ algoWeb.js
@@ -1,25 +1,89 @@
 var workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox')});
+var myInterpreter;
 
-var runCode = function() {
+var runButton = function() {
     var code = Blockly.JavaScript.workspaceToCode(workspace);
-    var myInterpreter = new Interpreter(code, initApi);
-    myInterpreter.run();
+    myInterpreter = new Interpreter(code, initApi);
+    var runCode = function () {
+        if(myInterpreter.run()) {
+            // Ran until an async call.  Give this call a chance to run.
+            // Then start running again later.
+            // 1000ms is waaay too long, but is used here to demo the pause.
+            setTimeout(runCode, 10);
+        };
+    };
+    runCode();
+};
+
+function displayText(text) {
+    var div = document.getElementById('console');
+    var t = document.createTextNode(text);
+    div.appendChild(t);
+}
+
+function displayTextLN(text) {
+    var div = document.getElementById('console');
+    var n = document.createTextNode(text);
+    div.appendChild(n);
+    n = document.createNode('br');
+    div.appendChild(n);
+}
+
+var promptId;
+var promptOk;
+
+function consolePrompt(text) {
+    var div = document.getElementById('console');
+
+    var n = document.createTextNode(text);
+    div.appendChild(n);
+
+    n = document.createElement('input');
+    n.setAttribute('type', 'text');
+    promptId = Date.now();
+    n.setAttribute('name', promptId);
+    n.setAttribute('id', 'input' + promptId);
+    div.appendChild(n);
+    n.focus();
+
+    n = document.createElement('button');
+    n.setAttribute('type', 'button');
+    n.setAttribute('onclick', 'promptOk()');
+    n.setAttribute('id', 'button' + promptId);
+
+    n.innerHTML = 'ok';
+    div.appendChild(n);
+
+    n = document.createElement('br');
+    div.appendChild(n);
 }
 
 function initApi(interpreter, scope) {
     // Add an API function for the alert() block.
     var wrapper = function(text) {
         text = text ? text.toString() : '';
-        return interpreter.createPrimitive(alert(text));
+        return interpreter.createPrimitive(displayText(text));
     };
+
     interpreter.setProperty(scope, 'alert',
                             interpreter.createNativeFunction(wrapper));
 
     // Add an API function for the prompt() block.
-    wrapper = function(text) {
+    wrapper = function(text,callback) {
         text = text ? text.toString() : '';
-        return interpreter.createPrimitive(prompt(text));
+        promptOk = function () {
+            var div = document.getElementById('console');
+            var input = document.getElementById('input'+promptId);
+            var button = document.getElementById('button'+promptId);
+
+            input.disabled = true;
+            button.disabled = true;
+
+            var value = input.value;
+            callback(interpreter.createPrimitive(value));
+        };
+        return consolePrompt(text);
     };
     interpreter.setProperty(scope, 'prompt',
-                            interpreter.createNativeFunction(wrapper));
-}
+                            interpreter.createAsyncFunction(wrapper));
+};
