3b05db0a604bcc3286f86310ae059dcba1211ae3
Rémi Vanicat <vanicat@debian.org>
2016-06-15 19:00:53 +0200

    Ajout de "lire_variable"

--- myblocks.js
+++ myblocks.js
@@ -1,6 +1,6 @@
 //https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#6f7hfn
 
-Blockly.Blocks['affectation'] = {
+Blockly.Blocks['variables_affectation'] = {
   init: function() {
     this.appendValueInput("value")
         .appendField(new Blockly.FieldVariable("variable"), "NAME")
@@ -13,10 +13,34 @@ Blockly.Blocks['affectation'] = {
   }
 };
 
-Blockly.JavaScript['affectation'] = function(block) {
+Blockly.JavaScript['variables_affectation'] = function(block) {
   var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
   var value_value = Blockly.JavaScript.valueToCode(block, 'value', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = variable_name + ' = ' + value_value + ';\n';
+  return code;
+};
+
+// https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#4okpf7
+
+Blockly.Blocks['variables_lire'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Lire")
+        .appendField(new Blockly.FieldVariable("variable"), "NAME");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(330);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.JavaScript['variables_lire'] = function(block) {
+  var display_name = block.getFieldValue('NAME');
+  var variable_name = Blockly.JavaScript.variableDB_.getName(display_name, Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble JavaScript into code variable.
+  var code = variable_name + ' = ' + 'parseFloat(prompt(\'' + display_name + ': \'));\n';
   return code;
 };
